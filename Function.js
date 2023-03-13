70ab5a74624183310fc08ce8596f445467e5ac3b
Sirimas2828 <biwsiri.k@gmail.com>
2023-03-13 16:50:58 +0700

    4.50 pm assignment

---
+++ Function.js
@@ -0,0 +1,33 @@
+// try creating function add/subtract/multiplay/divide
+
+const add = (a, b) =>{
+    console.log(a+b);
+    return (a+b);
+}
+
+const subtract = (a, b) =>{
+    console.log(a-b);
+    return (a-b);
+}
+
+const multiplay = (a, b) =>{
+    console.log(a*b);
+    return (a*b);
+}
+
+const divide = (a, b) =>{
+    console.log(a/b);
+    return (a/b);
+}
+
+
+add(3,4);
+subtract(3,4);
+multiplay(3,4);
+divide(3,4);
+
+// another exercise
+const firstName = 'Sirimas';
+const location = 'Chiang Mai';
+const hobby = 'playing games';
+console.log ("Hi, my name is "+firstName+".I live in "+location+" and enjoy "+hobby);
